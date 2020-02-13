1575a94f2b38a929c1daa440307478558643ea05
Justin <justin@maxset.org>
2020-02-13 15:04:45 -0800

    add viewbase to memory db

---
+++ internal/database/memory/viewbase.go
@@ -0,0 +1,27 @@
+package memory
+
+import (
+	"git.maxset.io/web/knaxim/internal/database"
+	"git.maxset.io/web/knaxim/internal/database/filehash"
+)
+
+type Viewbase struct {
+	Database
+}
+
+func (vb *Viewbase) Insert(vs *database.ViewStore) error {
+	lock.Lock()
+	defer lock.Unlock()
+	vb.Views[vs.ID.String()] = vs
+	return nil
+}
+
+func (vb *Viewbase) Get(id filehash.StoreID) (out *database.ViewStore, err error) {
+	lock.RLock()
+	defer lock.RUnlock()
+	out, ok := vb.Views[id.String()]
+	if !ok {
+		out = nil
+	}
+	return
+}
