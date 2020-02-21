39d51c1a36c0e3ee11f992f0daadc2e2f3541653
Devon Call <devon@maxset.org>
2020-02-21 13:33:44 -0800

    production version doesn't report errors

--- web/src/service/api.js
+++ web/src/service/api.js
@@ -3,9 +3,6 @@ import axios from 'axios'
 import VueAxios from 'vue-axios'
 
 const ApiService = {
-  // TODO: Is there another/better way to access the root Vue instance?!
-  // Expose the root vue instance for...
-  //  - sending global error events from ApiService (app::set::error)
   _vm: null,
 
   /**
@@ -17,67 +14,35 @@ const ApiService = {
     // they can be overridden with a similar file with a .local extension.
     Vue.axios.defaults.baseURL = process.env.VUE_APP_API_URL
     Vue.axios.defaults.withCredentials = true
-
-    // *** Interceptors appear to be added in a stack, LIFO.
-    // *** They also fail silently!
-
-    // response interceptor to attempt conversion of all json data.
-    // TODO: Remove this interceptor when the server starts sending the
-    // correct content-type.
-    Vue.axios.interceptors.response.use(function (response) {
-      try {
-        let data = JSON.parse(response.data)
-        response.data = data
-      } catch (e) {}
-      return response
-    }, function (error) {
-      return Promise.reject(error)
-    })
-
-    // Handle global API response error messages.
-    Vue.axios.interceptors.response.use(function (config) {
-      if (config.data && config.data.message === 'login') {
-        ApiService._vm.$root.$emit('app::set::error', 'Please Login.')
-        // TODO: Throwing an error here blindly prevents duplicate requests
-        // in some cases.  This may need to be handled better by the code
-        // making requests, or perhaps a different mechanisim is needed.
-        throw new Error('Login Required')
-      }
-      return config
-    })
-
-    // TODO: Remove this code when we are confident that objects are accepted
-    //  by the server;  It will clean things up a bit.  This might be a
-    //  performance decision.
-
-    // Request FromData: Convert object into FormData object,
-    //  remove undefined params.
-    Vue.axios.interceptors.request.use(function (config) {
-      if (config.data) {
-        let data = new FormData()
-        for (let key in config.data) {
-          if (typeof config.data[key] !== 'undefined') {
-            data.append(key, config.data[key])
+    Vue.axios.defaults.transformRequest = [
+      function (data, header) {
+        let fdata = new FormData()
+        for (let key in data) {
+          if (typeof data[key] !== 'undefined') {
+            fdata.append(key, data[key])
           }
         }
-        config.data = data
+        return fdata
       }
+    ]
 
-      return config
-    })
+    Vue.axios.defaults.transformResponse = [
+      function (data) {
+        try {
+          return JSON.parse(data)
+        } catch {
+          return data
+        }
+      }
+    ]
   },
 
   query (resource, params) {
-    return Vue.axios.get(resource, { params: params }).catch(error => {
-      throw new Error(`ApiService ${error}`)
-    })
+    return Vue.axios.get(resource, { params: params })
   },
 
   get (resource, slug = '') {
     return Vue.axios.get(`${resource}` + (slug ? `/${slug}` : ''))
-      .catch(error => {
-        throw new Error(`ApiService ${error}`)
-      })
   },
 
   post (resource, params) {
@@ -89,9 +54,7 @@ const ApiService = {
   },
 
   delete (resource, params) {
-    return Vue.axios.delete(resource, { data: params }).catch(error => {
-      throw new Error(`ApiService ${error}`)
-    })
+    return Vue.axios.delete(resource, { data: params })
   }
 }
 
