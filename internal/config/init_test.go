19909022dfa32c434a031e48f957499f5fa3b6f5
Devon Call <devon@maxset.org>
2020-02-27 15:48:00 -0800

    added basic tests for config

---
+++ internal/config/init_test.go
@@ -0,0 +1,26 @@
+package config
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+func TestLoadingConfig(t *testing.T) {
+	tmpfile, err := ioutil.TempFile("", "testconfig*.json")
+	if err != nil {
+		t.Fatal("unable to make temporary file ", err.Error())
+	}
+	defer os.Remove(tmpfile.Name())
+
+	if _, err := tmpfile.Write(testconfigjson); err != nil {
+		t.Fatal(err)
+	}
+	if err := tmpfile.Close(); err != nil {
+		t.Fatal(err)
+	}
+	if err := ParseConfig(tmpfile.Name()); err != nil {
+		t.Log(tmpfile.Name())
+		t.Fatal(err)
+	}
+}
