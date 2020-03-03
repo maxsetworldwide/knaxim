9a17898b2cfb2a620097fe6fd405089a799ffdc7
Devon Call <devon@maxset.org>
2020-03-02 18:27:55 -0800

    adjusted header to be more responsive on small screens
    fixes #99

--- web/src/App.vue
+++ web/src/App.vue
@@ -4,19 +4,17 @@
     <b-row>
       <b-col>
         <auth ref="auth" :passkey="resetkey"></auth>
-        <b-navbar class="app-header" toggleable="md">
-          <b-navbar-brand href="/" class="pr-5">
+        <b-navbar class="app-header">
+          <b-navbar-brand href="/" class="d-none d-md-block pr-5">
             <b-img src="~@/assets/logo.png" alt="Knaxim Logo" />
             Knaxim.com
           </b-navbar-brand>
-          <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-          <b-collapse id="nav-collapse" is-nav>
-            <!-- Search & History -->
-            <header-search />
 
-            <!-- Settings Nav -->
-            <header-settings @login="showLogin" />
-          </b-collapse>
+          <!-- Search & History -->
+          <header-search />
+
+          <!-- Settings Nav -->
+          <header-settings @login="showLogin" />
         </b-navbar>
       </b-col>
       <hr class="w-100 m-0" />
