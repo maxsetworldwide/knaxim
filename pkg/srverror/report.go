7f565e491cdfed4611b840538fdbb2c1a8fa091d
Justin <justin@maxset.org>
2020-05-11 11:55:09 -0700

    Inital error logging commit. Add error string method

---
+++ pkg/srverror/report.go
@@ -0,0 +1,110 @@
+package srverror
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+)
+
+// report.go contains behavior for logging server errors by sending an email to
+// the devs and by saving them to a file.
+
+// Returns a tab-indented human readable message containing information about
+// the given error, request, and response
+func LogString(err error, req *http.Request, res http.ResponseWriter) string {
+	var status string
+	switch cast := err.(type) {
+	case Error:
+		status = "Status Code: " + strconv.Itoa(cast.Status())
+	default:
+		status = ""
+	}
+
+	var cookies []interface{}
+	for _, cookie := range req.Cookies() {
+		cookies = append(cookies, cookie.String())
+	}
+
+	var responseHeaders []interface{}
+	for key, arr := range res.Header() {
+		responseHeaders = append(responseHeaders, key)
+		vals := []interface{}{}
+		for _, val := range arr {
+			vals = append(vals, val)
+		}
+		responseHeaders = append(responseHeaders, vals)
+	}
+
+	contentLength := fmt.Sprintf("Content Length: %d", req.ContentLength)
+
+	logFormat := []interface{}{
+		time.Now().Format(time.RFC1123),
+		[]interface{}{
+			"Error Message",
+			[]interface{}{
+				err.Error(),
+				status,
+			},
+		},
+		[]interface{}{
+			"Request",
+			[]interface{}{
+				"Path",
+				[]interface{}{
+					req.URL.String(),
+				},
+				"Method",
+				[]interface{}{
+					req.Method,
+				},
+				"Remote Address",
+				[]interface{}{
+					req.RemoteAddr,
+				},
+				contentLength,
+				"Cookies",
+				cookies,
+			},
+		},
+		[]interface{}{
+			"Response",
+			[]interface{}{
+				"Current Header Values",
+				responseHeaders,
+			},
+		},
+	}
+
+	var result strings.Builder
+	buildSections(logFormat, &result, 0)
+	return result.String()
+}
+
+// Recursive function for converting log format to string format.
+// Sections are skipped if their content is empty.
+func buildSections(sections []interface{}, result *strings.Builder, indent int) {
+	for _, content := range sections {
+		switch v := content.(type) {
+		case string:
+			if len(v) > 0 {
+				writeTabs(result, indent)
+				result.WriteString(v)
+				result.WriteRune('\n')
+			}
+		case []interface{}:
+			buildSections(v, result, indent+1)
+		default:
+			writeTabs(result, indent)
+			result.WriteString("Unrecognized data type")
+			result.WriteRune('\n')
+		}
+	}
+}
+
+func writeTabs(builder *strings.Builder, num int) {
+	for i := 0; i < num; i++ {
+		builder.WriteRune('\t')
+	}
+}
---
+++ pkg/srverror/report_test.go
@@ -0,0 +1,68 @@
+package srverror
+
+import (
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"strconv"
+	"strings"
+	"testing"
+	"time"
+)
+
+//TODO vanilla error test
+func TestReport(t *testing.T) {
+	t.Run("LogString", func(t *testing.T) {
+		testUrl := "/test/path"
+		testCookies := []http.Cookie{
+			http.Cookie{
+				Name:  "testCookieName",
+				Value: "testCookieValue",
+			},
+			http.Cookie{
+				Name:  "testCookieNameTwo",
+				Value: "testCookieValueTwo",
+			},
+		}
+		testError := New(errors.New("wrapped error"), 555, "extra message one", "extra message two")
+		testMethod := "POST"
+		testBody := "test body for purposes of giving content length to request"
+		testContentLength := len(testBody)
+		testReq, _ := http.NewRequest(testMethod, testUrl, strings.NewReader(testBody))
+		var expectedCookieContent []string
+		for _, c := range testCookies {
+			testReq.AddCookie(&c)
+			expectedCookieContent = append(expectedCookieContent, c.Value)
+		}
+		remoteAddr := "testRemoteAddr"
+		testReq.RemoteAddr = remoteAddr
+		testRes := httptest.NewRecorder()
+		testResHeadKey := "Testresponseheaderkey"
+		testResHeadVal := "testResponseHeaderValue"
+		testRes.Header().Add(testResHeadKey, testResHeadVal)
+		timestamp := time.Now().Format(time.RFC1123) // possible to not match actual
+		// time during call. If that becomes a problem, might be better to regex for
+		// existence of some timestamp. However, rerunning tests will generally work
+
+		expectedContent := []string{
+			timestamp,
+			testUrl,
+			testMethod,
+			strconv.Itoa(testError.Status()),
+			strconv.Itoa(testContentLength),
+			testError.Error(),
+			testResHeadKey,
+			testResHeadVal,
+			remoteAddr,
+		}
+		expectedContent = append(expectedContent, expectedCookieContent...)
+
+		result := LogString(testError, testReq, testRes)
+		t.Logf("Result log string: \n%s\n", result)
+		for _, expect := range expectedContent {
+			if strings.Index(result, expect) == -1 {
+				t.Errorf("Expected log string to contain '%s'", expect)
+			}
+		}
+	})
+}
