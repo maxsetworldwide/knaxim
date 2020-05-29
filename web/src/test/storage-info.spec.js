859a981d6d62633b4f45b8b4b44b71f4d3ea3461
devon <devon@maxset.org>
2020-05-29 13:50:23 -0700

    added Vuex for storage info

--- web/src/test/storage-info.spec.js
+++ web/src/test/storage-info.spec.js
@@ -1,5 +1,7 @@
-import { shallowMount } from '@vue/test-utils'
+import { shallowMount, createLocalVue } from '@vue/test-utils'
 import StorageInfo from '@/components/storage-info'
+import { ON, OFF } from '@/store/mutations.type'
+import Vuex from 'vuex'
 
 // API options for test-utils - mount, shallowMount, etc.:
 //   https://vue-test-utils.vuejs.org/api
@@ -10,8 +12,20 @@ import StorageInfo from '@/components/storage-info'
 // Jasmine matchers - toBeTruthy, toBeDefined, etc.
 //   https://jasmine.github.io/api/3.5/matchers.html
 
+const localVue = createLocalVue()
+localVue.use(Vuex)
+
+let store = new Vuex.Store({
+  mutations: {
+    [ON] () {},
+    [OFF] () {}
+  }
+})
+
 const shallowMountFa = (options = { props: {}, methods: {}, computed: {} }) => {
   return shallowMount(StorageInfo, {
+    localVue,
+    store,
     propsData: {
       ...options.props
     },
