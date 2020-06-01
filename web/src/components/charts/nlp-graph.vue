39006a693c6183ae258878738d706fa8cf0668ad
Justin <justin@maxset.org>
2020-06-01 15:19:37 -0700

    move nlp graph logic to its own component

---
+++ web/src/components/charts/nlp-graph.vue
@@ -0,0 +1,150 @@
+<!--
+ - nlp-graph: wrapper component for donut-complete for the use of displaying
+ -            nlp data from vuex. The parent can choose which tag to display.
+ -
+ - props:
+ -   fid: file id
+ -   type: choose which graph to show, specified within the data() field. Any
+ -         string within the props field of the type may be used to specify it,
+ -         e.g. 't', 'topic', and 'topics' might all be valid to choose Topic.
+ - events:
+ -   no-data: emitted when the data source of the chosen graph is empty.
+ - slots:
+ -   default: element appearing above the graph, usually for a title. Falls back
+ -            to a capitalized title of the chosen tag in an <h3> tag.
+ -->
+
+<template>
+  <div>
+    <slot>
+      <h3>{{ propToType.title }}</h3>
+    </slot>
+    <donut-complete
+      :dataVals="graphData"
+      :colors="propToType.colors"
+      @click="handleGraphClick($event)"
+    />
+  </div>
+</template>
+
+<script>
+import DonutComplete from '@/components/charts/donut-complete'
+import { Color } from '@/components/charts/presets'
+import { mapGetters } from 'vuex'
+// import { NLP_DATA } from '@/store/actions.type'
+
+export default {
+  name: 'nlp-graph',
+  components: {
+    DonutComplete
+  },
+  props: {
+    fid: {
+      type: String,
+      required: true
+    },
+    type: {
+      type: String,
+      required: true
+    }
+  },
+  data () {
+    return {
+      types: Object.freeze({
+        TOPIC: {
+          title: 'Topics',
+          tag: 'topic',
+          props: ['t', 'topic', 'topics'],
+          src: {
+            dataLocation: 'nlpTopics',
+            index: true // index into dataLocation via this.fid
+          },
+          colors: Color.Topics
+        },
+        ACTION: {
+          title: 'Actions',
+          tag: 'action',
+          props: ['a', 'action', 'actions'],
+          src: {
+            dataLocation: 'nlpActions',
+            index: true
+          },
+          colors: Color.Actions
+        },
+        RESOURCE: {
+          title: 'Resources',
+          tag: 'resource',
+          props: ['r', 'resource', 'resources'],
+          src: {
+            dataLocation: 'modifiedResources', // resources rely on topics
+            index: false
+          },
+          colors: Color.Resources
+        }
+      })
+    }
+  },
+  computed: {
+    // translate the given prop to a consistent type to use programmatically
+    propToType () {
+      const prop = this.type
+      for (let key in this.types) {
+        let type = this.types[key]
+        if (type.props.includes(prop)) {
+          return type
+        }
+      }
+      return null
+    },
+    graphSource () {
+      const src = this.propToType.src
+      if (src.index) {
+        return this[src.dataLocation][this.fid]
+      } else {
+        return this[src.dataLocation]
+      }
+    },
+    graphData () {
+      const data = this.graphSource.slice(0, 7)
+      let result = []
+      for (let val in data) {
+        let { word, count } = data[val]
+        result.push({
+          label: word,
+          data: count
+        })
+      }
+      return result
+    },
+    ...mapGetters(['nlpTopics', 'nlpActions', 'nlpResources']),
+    // resources will want to filter out words that exist in topics
+    modifiedResources () {
+      const topicType = this.types.TOPIC
+      const topicSrc = topicType.src.index ? this[topicType.src.dataLocation][this.fid] : this[topicType.src.dataLocation]
+      const topics = topicSrc.map((topic) => {
+        return topic.word || ''
+      }).filter((word) => {
+        return word !== ''
+      })
+      return this.nlpResources[this.fid].filter(({ word }) => {
+        return !topics.includes(word)
+      })
+    }
+  },
+  methods: {
+    handleGraphClick (label) {
+      const tag = this.propToType.tag
+      this.$router.push({ path: `/search/${label}/tag/${tag}` })
+    }
+  },
+  created () {
+    if (!this.graphSource.length) {
+      this.$emit('no-data')
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- web/src/components/file-side-graphs.vue
+++ web/src/components/file-side-graphs.vue
@@ -1,48 +1,29 @@
 <template>
   <div class="w-100">
     <b-spinner v-if="nlpLoading" />
-    <b-container v-else class="donut">
-      <b-row v-if="topicData.length > 0">
-        <h3>Topics</h3>
-        <donut-complete
-          class="w-100"
-          :dataVals="topicData"
-          :colors="topicColors"
-          @click="handleGraphClick('topic', $event)"
-        />
+    <b-container v-else>
+      <b-row v-if="graphsExist.topic">
+         <nlp-graph class="w-100" type="topic" :fid="fid" @no-data="graphsExist.topic = false" />
       </b-row>
-      <b-row v-if="actionData.length > 0">
-        <h3>Actions</h3>
-        <donut-complete
-          class="w-100"
-          :dataVals="actionData"
-          :colors="actionsColors"
-          @click="handleGraphClick('action', $event)"
-        />
+      <b-row v-if="graphsExist.action">
+         <nlp-graph class="w-100" type="action" :fid="fid" @no-data="graphsExist.action = false" />
       </b-row>
-      <b-row v-if="resourceData.length > 0">
-        <h3>Resources</h3>
-        <donut-complete
-          class="w-100"
-          :dataVals="resourceData"
-          :colors="resourceColors"
-          @click="handleGraphClick('resource', $event)"
-        />
+      <b-row v-if="graphsExist.resource">
+         <nlp-graph class="w-100" type="resource" :fid="fid" @no-data="graphsExist.resource = false" />
       </b-row>
     </b-container>
   </div>
 </template>
 
 <script>
-import DonutComplete from '@/components/charts/donut-complete'
-import { Color } from '@/components/charts/presets'
 import { mapGetters, mapActions } from 'vuex'
 import { NLP_DATA } from '@/store/actions.type'
+import NlpGraph from '@/components/charts/nlp-graph'
 
 export default {
   name: 'file-side-graphs',
   components: {
-    DonutComplete
+    NlpGraph
   },
   props: {
     fid: {
@@ -50,56 +31,19 @@ export default {
       required: true
     }
   },
+  data () {
+    return {
+      graphsExist: {
+        topic: true,
+        action: true,
+        resource: true
+      }
+    }
+  },
   computed: {
-    // TODO: this is repeated logic from file-preview. Move this to a renderless component.
-    topicColors () {
-      return Color.Topics
-    },
-    actionsColors () {
-      return Color.Actions
-    },
-    resourceColors () {
-      return Color.Resources
-    },
-    topicData () {
-      return this.buildGraphData(this.nlpTopics[this.fid])
-    },
-    actionData () {
-      return this.buildGraphData(this.nlpActions[this.fid])
-    },
-    resourceData () {
-      const topics = this.nlpTopics[this.fid]
-        .map((topic) => {
-          return topic.word || ''
-        })
-        .filter((word) => {
-          return word !== ''
-        })
-      return this.buildGraphData(
-        this.nlpResources[this.fid]
-          .filter(({ word }) => {
-            return !topics.includes(word)
-          })
-          .slice(0, 7)
-      )
-    },
-    ...mapGetters(['nlpTopics', 'nlpActions', 'nlpResources', 'nlpLoading'])
+    ...mapGetters(['nlpLoading'])
   },
   methods: {
-    buildGraphData (data) {
-      let result = []
-      for (let val in data) {
-        let { word, count } = data[val]
-        result.push({
-          label: word,
-          data: count
-        })
-      }
-      return result
-    },
-    handleGraphClick (tag, label) {
-      this.$router.push({ path: `/search/${label}/tag/${tag}` })
-    },
     ...mapActions([NLP_DATA])
   },
   created () {
