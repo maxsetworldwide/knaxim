2392c659f9b48476a5380e5268fea12606b51fdf
devon <devon@maxset.org>
2020-06-15 11:44:25 -0700

    added navigation buttons to image viewer

--- web/src/components/image-viewer.vue
+++ web/src/components/image-viewer.vue
@@ -1,7 +1,12 @@
 <template>
   <b-container class="image-viewer">
     <b-row class="image-toolbar">
-      <b-col offset="3" cols="6" offset-md="4" md="4">
+      <b-col class="d-none d-md-flex" md="1">
+        <b-button v-if="navigation.prev" @click="openPrev" class="min-width-1em">
+          <b-icon icon="arrow-bar-left" class="icon" />
+        </b-button>
+      </b-col>
+      <b-col offset="2" cols="6" offset-md="3" md="4">
         <h4 class="title text-center">{{ fileInfo.name }}</h4>
       </b-col>
       <b-col cols="2" md="1">
@@ -11,6 +16,11 @@
           disableDownloadPDF
           :checkedFiles="[fileInfo]"
         />
+      </b-col>
+      <b-col class="d-none d-md-flex" offset-md="2" md="1">
+        <b-button v-if="navigation.next" @click="openNext" class="min-width-1em">
+          <b-icon icon="arrow-bar-right" class="icon" />
+        </b-button>
       </b-col>
     </b-row>
     <b-row class="content-row h-100" align-h="center">
@@ -47,12 +57,38 @@ export default {
     fileInfo () {
       return this.populateFiles(this.id)
     },
-    ...mapGetters(['populateFiles'])
+    navigation () {
+      let nav = {
+        prev: null,
+        next: null
+      }
+      let indx = this.activeFiles.findIndex(af => af === this.id)
+      if (indx > 0) {
+        nav.prev = this.activeFiles[indx - 1]
+      }
+      if (indx >= 0 && indx < this.activeFiles.length - 1) {
+        nav.next = this.activeFiles[indx + 1]
+      }
+      return nav
+    },
+    ...mapGetters(['populateFiles', 'activeFiles'])
+  },
+  methods: {
+    openPrev () {
+      if (this.navigation.prev) {
+        this.$router.push(`/file/${this.navigation.prev}`)
+      }
+    },
+    openNext () {
+      if (this.navigation.next) {
+        this.$router.push(`/file/${this.navigation.next}`)
+      }
+    }
   }
 }
 </script>
 
-<style>
+<style scoped lang="scss">
 .title {
   width: 100%;
   white-space: nowrap;
@@ -74,5 +110,19 @@ export default {
 
 .image-content {
   object-fit: contain;
+}
+
+button {
+  background-color: white;
+  border-radius: 10px;
+  border: 0px;
+  width: 100%;
+  height: 30px;
+  color: rgb(46, 46, 46);
+}
+
+button:hover {
+  background-color: rgb(150, 182, 252);
+  color: rgb(46, 46, 46);
 }
 </style>
