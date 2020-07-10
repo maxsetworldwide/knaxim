4f20115a808ea37ee95c126a4d807e9d17363cad
devon <devon@maxset.org>
2020-07-09 21:02:55 -0700

    finished database

--- internal/database/types/errors/errors.go
+++ internal/database/types/errors/errors.go
@@ -1,3 +1,22 @@
+/*************************************************************************
+ *
+ * MAXSET CONFIDENTIAL
+ * __________________
+ *
+ *  [2019] - [2020] Maxset WorldWide Inc.
+ *  All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Maxset WorldWide Inc. and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Maxset WorldWide Inc.
+ * and its suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Maxset WorldWide Inc.
+ */
+
 package errors
 
 import (
