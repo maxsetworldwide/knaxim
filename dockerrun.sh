0bee8772fc91e22703cc5cdcd15e57d9b5c66ded
devon <devon@maxset.org>
2020-07-16 17:48:59 -0700

    updated system to use knaximctl instead of acronymloader

--- dockerrun.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-
-echo "Knaxim Start Mode: '$KNAXIMINIT'"
-if [ "$KNAXIMINIT" = "restart" ]; then
-  /bin/knaximctl 
-  /bin/knaximctl -init -db mongodb://mongo:27017 -dur 2m -f /resource/DoDIndex.csv
-  /bin/knaximctl -db mongodb://mongo:27017 -dur 2m -f /resource/NextDODIndex.csv
-fi
